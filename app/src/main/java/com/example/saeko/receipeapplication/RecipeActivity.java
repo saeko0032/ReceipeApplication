da58e112f8fbcd3b4fab88b79862d3fa022a6eab
Saeko Fukui <saeko0032@gmail.com>
2017-07-24 12:32:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/saeko/receipeapplication/RecipeActivity.java
@@ -0,0 +1,13 @@
+package com.example.saeko.receipeapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
